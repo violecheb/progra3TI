9df13e52b3d81e9477266b10e161114b40d356f0
dmasrii <dmasri@udesa.edu.ar>
2025-03-31 12:50:31 -0300

    Ruta home, detalle y Error404

--- proyecto-integrador/src/App.js
+++ proyecto-integrador/src/App.js
@@ -1,6 +1,17 @@
+import React from "react";
+import {Switch,Route} from 'react-router-dom'
+import { Router } from "react-router-dom/cjs/react-router-dom.min";
+// Aca va la importación de los screens cuando los tengamos
+
+
+
 function App() {
   return (
-    <p> React</p>
+    <Switch> 
+      <Route path = "/" exact = {true} component = {Home}/>
+      <Route path = "/detalle/:id" component = {Detalle}/>
+      <Route component= {Error404} />
+    </Switch>
   );
  }
  
