623db64d5df904791f59a544861f420b5327d9cc
dmasrii <dmasri@udesa.edu.ar>
2025-04-07 18:04:30 -0300

    Screen de error 404 terminado

---
+++ proyecto-integrador/src/Screens/Error/Error.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Error(){
+    return (
+        <div> 
+            <h2> Error 404</h2>
+            <p>Lo sentimos, la pagina buscada no existe!</p>
+        </div>
+    )
+}
+export default Error
