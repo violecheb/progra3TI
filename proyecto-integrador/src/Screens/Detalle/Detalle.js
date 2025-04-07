7b51fbf8875c212b25516f4182d73e3663be0979
dmasrii <dmasri@udesa.edu.ar>
2025-04-07 17:54:10 -0300

    terminado detalle falta agregarle cosas y css

--- proyecto-integrador/src/Screens/Detalle/Detalle.js
+++ proyecto-integrador/src/Screens/Detalle/Detalle.js
@@ -1,19 +1,22 @@
 import React, {Component} from "react";
 
+
 class Detalle extends Component{
     constructor(props){
         super(props)
         this.state ={
-            movie : null
+            movie : null,
+            Cargando: true
         }
     }
     componentDidMount(){
         let id = this.props.match.params.id
-        fetch(`https://api.themoviedb.org/3/movie/${id}`)
+        fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=16165a70d46ac6b42f11100b26969ebb`)
         .then((response) => response.json())
         .then ((data) => {
             this.setState({
-                movie:data
+                movie:data,
+                Cargando: false
             })
         })
         .catch((error) => {
@@ -22,26 +25,21 @@ class Detalle extends Component{
         
     }
     render(){
-            let {movie} = this.state;
-            if (!movie) return <h2> Cargando...</h2>
+            let {movie, Cargando } = this.state;
+            if (Cargando ) return <h2> Cargando...</h2>
         return(
             <div> 
-                <h2>{movie.title}</h2>
-                <img src = {movie.img}/>
+                <h2>Titulo: {movie.title}</h2>
+                <img src = {`https://image.tmdb.org/t/p/w342/${movie.poster_path}`}/>
                 <p>Calificación:{movie.vote_average}</p>
                 <p>Fecha de estreno:{movie.release_date}</p>
-                <p>Duración{movie.runtime} minutos</p>
+                <p>Duración minutos:{movie.runtime} </p>
                 <p>Sinopsis: {movie.overview}</p>
-                <p>Genero: {movie.genres.map((genre) => genre.name)}</p>
+                <p>Genero: {movie.genres.map((genre) => genre.name).join(', ')}</p>
             </div>
         )
 
     }
-    
-    
-            
-        
-    
 
 }
 
