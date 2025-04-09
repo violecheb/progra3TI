f5ba140ab56a37e9ae5fabc1c2bc891907c842bd
dmasrii <dmasri@udesa.edu.ar>
2025-04-09 13:59:23 -0300

    Resultados busqueda terminado

--- proyecto-integrador/src/Screens/SearchResults/SearchResults.js
+++ proyecto-integrador/src/Screens/SearchResults/SearchResults.js
@@ -1,10 +1,43 @@
-import React from "react";
+import React, {Component} from "react";
+import SearchForm from "../../Components/SearchForm/SearchForm";
+import Loader from "../../Components/Loader/Loader";
+import MoviesList from "../../Components/MoviesList/MoviesList";
 
-function SearchResults(){
-    return (
-        <div> 
-            <h2> soy los resultados delfi</h2>
-        </div>
-    )
+class SearchResults extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            resultados: [],
+            recibido: false
+        }
+    }
+
+    componentDidMount(){
+        const busqueda = this.props.match.params.busqueda
+        fetch(`https://api.themoviedb.org/3/search/movie?api_key=16165a70d46ac6b42f11100b26969ebb&query=${busqueda}`)
+        .then( response => response.json() )
+        .then( data => this.setState(
+            {resultados: data.results,
+            recibido: true
+            } //recibe un objeto literal
+        ))
+        .catch( error => console.log(error) )}
+    
+    render(){
+        const resultados = this.state.resultados
+        console.log("resultados", resultados)
+        return(
+            <main>
+            {!this.state.recibido ? 
+            <>
+            <Loader/> 
+            </>: this.state.resultados.length == 0 ? <h1>No se encontraron peliculas</h1>:
+            <MoviesList header= "Resultados de busqueda" movies = {this.state.resultados}/>
+            }
+            </main>
+        )
+    }
+
 }
+
 export default SearchResults;
