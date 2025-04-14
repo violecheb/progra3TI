3bd78a6877d0437146734dba261b1d5fce180d24
pilarantunez <pantunezmunoz@udesa.edu.ar>
2025-04-14 10:11:36 -0300

    arreglando errores

--- proyecto-integrador/src/App.js
+++ proyecto-integrador/src/App.js
@@ -3,7 +3,7 @@ import {Switch,Route} from 'react-router-dom'
 //import Favoritos from "./Screens/Favoritos/Favoritos";
 import Detalle from "./Screens/Detalle/Detalle"
 import Header from "./Components/Header/Header"
-import Error from "./Screens/Error/Error"
+import Error404 from "./Screens/Error/Error"
 import Home from "./Screens/Home/Home"
 import Footer from "./Components/Footer/Footer";
 import TopRated from "./Screens/TopRated/TopRated";
@@ -20,11 +20,11 @@ function App() {
     <Switch> 
       <Route path = "/" exact = {true} component = {Home}/>
       <Route path = "/detalle/:id" component = {Detalle}/>
-      <Route path="/toprated" component= {TopRated} />
+      <Route path="/topRated" component= {TopRated} />
       <Route path="/searchresults/:busqueda" component={SearchResults}/>
       <Route path="/upcoming" component={Upcoming} />
       <Route path="/favorites" component={Favorites}/>
-      <Route component= {Error} />
+      <Route component= {Error404} />
     </Switch>
     <Footer />
     </>
